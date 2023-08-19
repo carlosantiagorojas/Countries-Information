54b37eed263604b69df08ed18eecc6ff7a2fd78c
unknown <carlosantirc@gmail.com>
2023-08-19 18:08:23 -0500

    Initial adapter

--- app/src/main/java/com/puj/countries_information/activities/MainActivity.kt
+++ app/src/main/java/com/puj/countries_information/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.puj.countries_information
+package com.puj.countries_information.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.puj.countries_information.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/puj/countries_information/adapters/CountryAdapter.java
@@ -0,0 +1,4 @@
+package com.puj.countries_information.adapters;
+
+public class CountryAdapter {
+}
